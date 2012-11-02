1630632dc14723683c1c2e84dd33e8f1c4671779
Luke van der Hoeven <hungerandthirst@gmail.com>
2012-11-02 14:30:39 -0400

    frames for bowling
    subclass of LinkedList::Node

---
+++ frame.rb
@@ -0,0 +1,57 @@
+require_relative 'bowling'
+
+class Bowling::Frame < LinkedList::Node
+  class InvalidRoll < StandardError ; end
+  NotRolled = Class.new
+
+  MAX_PINS = 10
+
+  def initialize
+    super self
+  end
+
+  def rolled(pins_hit)
+    raise InvalidRoll, "#{pins_hit} is greater than max number of pins (#{MAX_PINS})" if pins_hit > MAX_PINS
+    raise InvalidRoll, "#{pins_hit} is greater than number of pins left (#{pins_left})" if pins_hit > pins_left
+
+    if first_roll == NotRolled
+      @first_roll = pins_hit
+    elsif !strike? && second_roll == NotRolled
+      @second_roll = pins_hit
+    else
+      raise InvalidRoll, 'all turns rolled for this frame'
+    end
+  end
+
+  # special accessor methods
+  %w(first_roll second_roll).each do |name|
+    define_method name.to_sym do
+      instance_variable_get("@#{name}".to_sym) || NotRolled
+    end
+  end
+
+  def total_pins_hit
+    return 0 if first_roll == NotRolled
+    frame_total
+  end
+
+  def frame_total
+    (strike? || second_roll == NotRolled) ? first_roll : first_roll + second_roll
+  end
+
+  def pins_left
+    MAX_PINS - total_pins_hit
+  end
+
+  def strike?
+    first_roll == MAX_PINS
+  end
+
+  def spare?
+    second_roll != NotRolled && (first_roll + second_roll) == MAX_PINS
+  end
+
+  def complete?
+    first_roll != NotRolled && (strike? || second_roll != NotRolled)
+  end
+end
---
+++ test_frame.rb
@@ -0,0 +1,73 @@
+require 'minitest/autorun'
+
+require_relative 'frame'
+
+class TestFrame < MiniTest::Unit::TestCase
+  def setup
+    @frame = Bowling::Frame.new
+  end
+
+  def test_sets_proper_roll
+    @frame.rolled(4)
+
+    assert_equal 4, @frame.first_roll
+    assert_equal Bowling::Frame::NotRolled, @frame.second_roll
+
+    refute @frame.complete?, "frame improperly complete"
+
+    @frame.rolled(5)
+    assert_equal 4, @frame.first_roll
+    assert_equal 5, @frame.second_roll
+
+    assert @frame.complete?, "frame not complete"
+
+    assert_raises Bowling::Frame::InvalidRoll do
+      @frame.rolled(9)
+    end
+  end
+
+  def test_knows_total_pins_hit
+    assert_equal 0, @frame.total_pins_hit
+
+    @frame.rolled(4)
+    @frame.rolled(5)
+
+    assert_equal 9, @frame.total_pins_hit
+  end
+
+  def test_knows_it_is_a_strike
+    @frame.rolled(10)
+
+    assert @frame.strike?, "not a strike"
+    refute @frame.spare?, "invalid spare"
+  end
+
+  def test_strike_cant_roll_second_roll
+    @frame.rolled(10)
+
+    assert_raises Bowling::Frame::InvalidRoll do
+      @frame.rolled(1)
+    end
+  end
+
+  def test_cant_roll_more_than_total_number_of_pins
+    assert_raises Bowling::Frame::InvalidRoll do
+      @frame.rolled(22)
+    end
+
+    assert_raises Bowling::Frame::InvalidRoll do
+      @frame.rolled(5)
+      @frame.rolled(6)
+    end
+  end
+
+  def test_knows_number_of_pins_left
+    assert_equal 10, @frame.pins_left
+
+    @frame.rolled(5)
+    assert_equal 5, @frame.pins_left
+
+    @frame.rolled(4)
+    assert_equal 1, @frame.pins_left
+  end
+end
